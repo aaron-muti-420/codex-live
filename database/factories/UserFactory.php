84dab6eac90b57474d9210ec70c7a51f6da37d67
hpmouton <hubertthedev@gmail.com>
2024-02-05 09:48:54 +0200

    User model updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,9 @@ class UserFactory extends Factory
         $role = Role::inRandomOrder()->first();
 
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->randomElement(['m','f']),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
