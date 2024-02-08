473649bdf6b2f1989c99f999d4dc61cc291857be
hpmouton <hubertthedev@gmail.com>
2024-02-08 09:21:29 +0200

    User Profile Update

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -1,44 +1,53 @@
 <x-app-layout>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 container mx-auto px-4 py-8 dark:bg-gray-900 dark:text-white">
-        <nav class="text-sm mb-4">
-            <ol class="list-none p-0 inline-flex">
-                <li class="flex items-center">
-                    <a href="/" class="text-blue-500 hover:text-blue-700">Home</a>
-                    <svg class="h-5 w-auto text-gray-400 mx-2" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1-1.414 1.414L11 4.414V15a1 1 0 1 1-2 0V4.414L5.707 7.707a1 1 0 0 1-1.414 0z" clip-rule="evenodd" />
-                    </svg>
-                </li>
-                <li class="flex items-center">
-                    <a href="/employees" class="text-blue-500 hover:text-blue-700">Employees</a>
-                    <svg class="h-5 w-auto text-gray-400 mx-2" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1-1.414 1.414L11 4.414V15a1 1 0 1 1-2 0V4.414L5.707 7.707a1 1 0 0 1-1.414 0z" clip-rule="evenodd" />
-                    </svg>
-                </li>
-                <li class="flex items-center">
-                    <span class="text-gray-500">Profile</span>
-                </li>
-            </ol>
-        </nav>
-
-        <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6">
-            <h1 class="text-2xl font-semibold mb-4">Employee Profile</h1>
-            <div class="flex items-center m-4 ">
-                @if ($user->profile_photo_path)
-                    <img src="{{ $user->profile_photo_path }}" alt="User Avatar" class="w-16 h-16 rounded-full mr-4">
-                @else
-                    <div class="w-16 h-16 rounded-full text-white flex items-center justify-center mr-4" style="background-color: {{ sprintf('#%06X', mt_rand(0, 0xFFFFFF)) }}">
-                        <span class="text-lg font-semibold">{{ substr($user->first_name, 0, 1) }}{{ substr($user->last_name, 0, 1) }}</span>
-                    </div>
-                @endif
-                <div class="ml-4">
-                    <h2 class="text-lg font-semibold">{{ $user->first_name }} {{ $user->last_name}}</h2>
-
-                    <p class="text-gray-500">{{ $user->email }}</p>
+    <div class="bg-white md:mx-auto rounded-lg shadow-xl w-full mt-10 md:w-1/2 overflow-hidden">
+        <div class="h-[140px] bg-gradient-to-r from-red-500 to-rose-500"></div>
+        <div class="px-5 py-2 flex flex-col gap-3 pb-6">
+        <div class="h-[90px] shadow-md w-[90px] rounded-full border-4 overflow-hidden -mt-14 border-white">
+            @if ($user->profile_photo_path)
+                <img src="{{ asset($user->profile_photo_path) }}" class="w-full h-full rounded-full object-center object-cover">
+            @else
+                <div class="flex items-center justify-center w-full h-full rounded-full bg-red-300 text-black text-2xl font-bold">
+                    {{ substr($user->first_name, 0, 1) }}{{ substr($user->last_name, 0, 1) }}
                 </div>
-            </div>
-            <div>
-                <h3 class="text-lg font-semibold mt-6">Personal Information</h3>
-            </div>
+            @endif
         </div>
-    </div>
+          <div class="">
+            <h3 class="text-xl text-slate-900 relative font-bold leading-6">{{ $user->first_name}} {{$user->last_name}}</h3>
+            <p class="text-sm text-gray-600">{{$user->position}}</p>
+          </div>
+          <div class="flex gap-3 flex-wrap"><span class="rounded-sm bg-yellow-100 px-3 py-1 text-xs font-medium text-yellow-800">{{$user->email}}</span><span class="rounded-sm bg-green-100 px-3 py-1 text-xs font-medium text-green-800">{{$user->role->role_title}}</span></div>
+          <h4 class="text-md font-medium leading-3">About</h4>
+          <p class="text-sm text-stone-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere dolores aliquid sequi sunt iusto ipsum earum natus omnis asperiores architecto praesentium dignissimos pariatur, ipsa cum? Voluptate vero eius at voluptas?</p>
+          <h4 class="text-md font-medium leading-3">Experiences</h4>
+          <div class="flex flex-col gap-3">
+            <div class="flex items-center gap-3 px-2 py-3 bg-white rounded border w-full "><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="h-8 w-8 text-slate-500" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"></path>
+              </svg>
+              <div class="leading-3">
+                <p class=" text-sm font-bold text-slate-700">Ui Designer</p><span class="text-xs text-slate-600">5 years</span>
+              </div>
+              <p class="text-sm text-slate-500 self-start ml-auto">As Ui Designer on Front Page</p>
+            </div>
+            <div class="flex items-center gap-3 px-2 py-3 bg-white rounded border w-full "><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="h-8 w-8 text-slate-500" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"></path>
+              </svg>
+              <div class="leading-3">
+                <p class=" text-sm font-bold text-slate-700">Ui Designer</p><span class="text-xs text-slate-600">5 years</span>
+              </div>
+              <p class="text-sm text-slate-500 self-start ml-auto">As Ui Designer on Front Page</p>
+            </div>
+            <div class="flex items-center gap-3 px-2 py-3 bg-white rounded border w-full "><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="h-8 w-8 text-slate-500" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"></path>
+              </svg>
+              <div class="leading-3">
+                <p class=" text-sm font-bold text-slate-700">Ui Designer</p><span class="text-xs text-slate-600">5 years</span>
+              </div>
+              <p class="text-sm text-slate-500 self-start ml-auto">As Ui Designer on Front Page</p>
+            </div>
+          </div>
+        </div>
+      </div>
 </x-app-layout>
